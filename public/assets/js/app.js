67bc56be6132f927c4af0ae1cb9f2207031ac998
rob <rob@bit-academy.nl>
2025-05-14 12:56:44 +0200

    Version 2

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,14 @@
 "use strict";
-
-/**
- * load PWA
- */
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker
-            .register("/serviceWorker.js")
-            .then(res => console.log("service worker registered"))
-            .catch(err => console.error("service worker not registered", err))
-    })
-}
+//
+// /**
+//  * load PWA
+//  */
+// if ("serviceWorker" in navigator) {
+//     window.addEventListener("load", function() {
+//         navigator.serviceWorker
+//             .register("/serviceWorker.js")
+//             .then(res => console.log("service worker registered"))
+//             .catch(err => console.error("service worker not registered", err))
+//     })
+// }
 
